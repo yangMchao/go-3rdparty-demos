f1641c339af79c429d0dca95cb411ff8e0bfd56c
yangchao <yangchao@zetyun.com>
2025-08-14 16:38:38 +0800

    zap test

--- chapter-03-log/zap/log_level_test.go
+++ chapter-03-log/zap/log_level_test.go
@@ -14,7 +14,7 @@ func TestLogLevel(t *testing.T) {
 	logger.Debug("Debugging information") // 在生产级别不会被记录
 	logger.Info("Information message")    // 将被记录
 	logger.Warn("Warning message")        // 将被记录
-	logger.Error("Error message", // 将被记录，并包含堆栈跟踪
+	logger.Error("Error message",         // 将被记录，并包含堆栈跟踪
 		zap.Error(errors.New("sample error")),
 	)
 	// logger.Fatal("Fatal error") // 将记录并终止程序
